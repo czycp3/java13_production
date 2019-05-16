2f1eb966108326c5fa3d3bb00c85193dc08b3f01
tuixiaofei <tuixiaofei@icloud.com>
2019-05-16 23:01:34 +0800

    test

---
+++ mybatis_crud/src/main/java/com/tuixiaofei/domain/QueryVo.java
@@ -0,0 +1,19 @@
+package com.tuixiaofei.domain;
+
+/**
+ * @Author: 頽小废
+ * @Date: 2019/5/14 11:53
+ * @Compony: http://www.tuixiaofei.com
+ */
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
