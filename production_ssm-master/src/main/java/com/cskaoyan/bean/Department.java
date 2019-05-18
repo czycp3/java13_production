6d38306538e0423429dc9e310096f64501d9bb72
tuixiaofei <tuixiaofei@icloud.com>
2019-05-18 16:44:52 +0800

    更新部门管理模块

---
+++ production_ssm-master/src/main/java/com/cskaoyan/bean/Department.java
@@ -0,0 +1,49 @@
+package com.cskaoyan.bean;
+
+import java.io.Serializable;
+
+/**
+ * @Author: 頽小废
+ * @Date: 2019/5/17 15:48
+ * @Compony: http://www.tuixiaofei.com
+ *
+ * 部门管理
+ */
+public class Department implements Serializable {
+    /**
+     * 部门编号
+     */
+    private String departmentId;
+    /**
+     * 部门名称
+     */
+    private String departmentName;
+    /**
+     * 部门职责
+     */
+    private String note;
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
