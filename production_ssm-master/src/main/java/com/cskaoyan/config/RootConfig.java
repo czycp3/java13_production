57bbbca36628b09706193bb118c2ab7945b2d8dc
DanIsADreamer <dansdream@163.com>
2019-05-19 18:22:26 +0800

    Update RootConfig.java

--- production_ssm-master/src/main/java/com/cskaoyan/config/RootConfig.java
+++ production_ssm-master/src/main/java/com/cskaoyan/config/RootConfig.java
@@ -42,9 +42,9 @@ public class RootConfig {
 	public DataSource dataSource() {
 		DruidDataSource dataSource = new DruidDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/project1?serverTimezone=GMT&useSSL=false&rewriteBatchedStatements=true&useServerPrepStmts=true&cachePrepStmts=true");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/production_ssm?serverTimezone=GMT&useSSL=false&rewriteBatchedStatements=true&useServerPrepStmts=true&cachePrepStmts=true");
 		dataSource.setUsername("root");
-		dataSource.setPassword("mysql520520");
+		dataSource.setPassword("123456");
 		return dataSource;
 	}
 
