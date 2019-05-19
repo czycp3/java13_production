09536cb1058be168d8fd0612bae5c2a4214f3290
czycp3 <czycp3@gmail.com>
2019-05-19 16:53:15 +0800

    提交人：CZY 终于把订单写完了

--- production_ssm-master/src/main/java/com/cskaoyan/controller/OrderController.java
+++ production_ssm-master/src/main/java/com/cskaoyan/controller/OrderController.java
@@ -3,9 +3,11 @@ package com.cskaoyan.controller;
 import com.cskaoyan.bean.BaseResultVo;
 import com.cskaoyan.bean.Order;
 import com.cskaoyan.bean.QueryStatus;
+import com.cskaoyan.exception.OrderException;
 import com.cskaoyan.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,7 +16,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.UUID;
+
 
 /**
  * @author CZY-Y7000P
@@ -32,12 +34,12 @@ public class OrderController {
     /*********订单展示************/
     @RequestMapping("/order/list")
     @ResponseBody
-    public BaseResultVo product(int page, int rows){
+    public BaseResultVo order(int page, int rows){
         return orderService.selectAllOrder(page, rows);
     }
 
     @RequestMapping("/order/find")
-    public String productList(){
+    public String orderList(){
         return "order_list";
     }
 
@@ -113,5 +115,73 @@ public class OrderController {
             e.printStackTrace();
         }
         return new QueryStatus();
+    }
+
+    /********编辑订单*******************/
+
+    @RequestMapping("/order/edit_judge")
+    @ResponseBody
+    public QueryStatus editJudge(){
+        return new QueryStatus();
+    }
+
+    @RequestMapping("/order/edit")
+    public String edit(){
+        return "order_edit";
+    }
+
+    @RequestMapping("/order/update_all")
+    @ResponseBody
+    public QueryStatus updateAll(Order order){
+        return orderService.updateByPrimaryKeySelective(order);
+    }
+
+    /**********删除订单*****************/
+
+    @RequestMapping("/order/delete_batch")
+    @ResponseBody
+    public QueryStatus deleteBatch(String[] ids){
+        try {
+            return orderService.deleteBatch(ids);
+        } catch (OrderException e) {
+            QueryStatus queryStatus = new QueryStatus();
+            queryStatus.setStatus(0);
+            queryStatus.setMsg(e.getMessage());
+            return queryStatus;
+        }
+    }
+
+    @RequestMapping("/order/delete_judge")
+    @ResponseBody
+    public QueryStatus deleteJudge(){
+        return new QueryStatus();
+    }
+
+    /**********查询产品*****************/
+
+    @RequestMapping("/order/search_order_by_orderId")
+    @ResponseBody
+    public BaseResultVo searchProductById(String searchValue,int page, int rows){
+        return orderService.searchOrderById(searchValue,page,rows);
+    }
+
+    @RequestMapping("/order/search_order_by_orderCustom")
+    @ResponseBody
+    public BaseResultVo searchOrderByCustomName(String searchValue,int page, int rows){
+        return orderService.searchOrderByCustomName(searchValue,page,rows);
+    }
+
+    @RequestMapping("/order/search_order_by_orderProduct")
+    @ResponseBody
+    public BaseResultVo searchOrderByProductName(String searchValue,int page, int rows){
+        return orderService.searchOrderByProductName(searchValue,page,rows);
+    }
+
+    /**********查询其它表的详情****************/
+
+    @RequestMapping("/order/get/{orderId}")
+    @ResponseBody
+    public Order searchOrderDetail(@PathVariable("orderId") String orderId){
+        return orderService.searchOrderDetail(orderId);
     }
 }
