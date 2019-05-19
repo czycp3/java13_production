dea9386662c02f39dd73323369eef8a7c3d2854f
DanIsADreamer <dansdream@163.com>
2019-05-19 18:19:03 +0800

    XOXO

--- production_ssm-master/src/main/java/com/cskaoyan/controller/FCountCheckController.java
+++ production_ssm-master/src/main/java/com/cskaoyan/controller/FCountCheckController.java
@@ -1,7 +1,10 @@
 package com.cskaoyan.controller;
 
+import com.cskaoyan.bean.BaseResultVo;
+import com.cskaoyan.bean.FCountCheck;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @auther 芮狼Dan
@@ -11,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FCountCheckController {
 
+    //新建提交的json对象
+    BaseResultVo baseResultVo=new BaseResultVo();
+
+    //返回f_count_check_list
     @RequestMapping("f_count_check/find")
     public String find(){
         return "f_count_check_list";
+    }
+
+    //分页查找
+    @RequestMapping("f_count_check/list")
+    @ResponseBody
+    public BaseResultVo<FCountCheck> findFCountCheckList(Integer page, Integer rows){
+
+
+        return baseResultVo;
     }
 }
