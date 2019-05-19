012a85c561196925737b3db7c7d9c6a69b5b7a5d
tuixiaofei <tuixiaofei@icloud.com>
2019-05-19 17:39:56 +0800

    更新部门管理模块

---
+++ production_ssm-master/src/main/java/com/cskaoyan/exception/DepartmentException.java
@@ -0,0 +1,15 @@
+package com.cskaoyan.exception;
+
+/**
+ * @Author: 頽小废
+ * @Date: 2019/5/19 14:21
+ * @Compony: http://www.tuixiaofei.com
+ */
+public class DepartmentException extends Exception {
+    public DepartmentException() {
+    }
+
+    public DepartmentException(String message) {
+        super(message);
+    }
+}
