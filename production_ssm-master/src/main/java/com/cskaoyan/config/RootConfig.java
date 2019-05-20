a9689fe3bbcc8f49d27d1b59e8d04c5ee030d8e1
czycp3 <czycp3@gmail.com>
2019-05-20 20:06:52 +0800

    提交人CZY 加了登录模块的最新版，别搞事情了

---
+++ production_ssm-master/src/main/java/com/cskaoyan/config/RootConfig.java
@@ -0,0 +1,77 @@
+package com.cskaoyan.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author CZY
+ */
+@Configuration
+@ImportResource("classpath:application.xml")
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.cskaoyan",
+		excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class,EnableWebMvc.class})})
+public class RootConfig {
+	/**
+	 * sqlSessionFactoryBean
+	 * @param dataSource
+	 * @return sqlSessionFactoryBean
+	 */
+	@Bean("sqlSessionFactoryBean")
+	public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource){
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setTypeAliasesPackage("com.cskaoyan.bean");
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		return sqlSessionFactoryBean;
+	}
+
+	/**
+	 * datasource
+	 * @return dataSource
+	 */
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/production_ssm?serverTimezone=GMT&useSSL=false&rewriteBatchedStatements=true&useServerPrepStmts=true&cachePrepStmts=true");
+		dataSource.setFilters("stat");
+		dataSource.setUsername("root");
+		dataSource.setPassword("123456");
+		return dataSource;
+	}
+
+	/**
+	 * MapperScannerConfigurer
+	 * @return MapperScannerConfigurer
+	 */
+	@Bean
+	public MapperScannerConfigurer mapperScannerConfigurer(){
+		MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+		mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
+		mapperScannerConfigurer.setBasePackage("com.cskaoyan.mapper");
+		return mapperScannerConfigurer;
+	}
+
+	/**
+	 * 事务管理器
+	 * @param dataSource
+	 * @return DataSourceTransactionManager
+	 */
+	@Bean
+	public DataSourceTransactionManager transactionManager(DataSource dataSource){
+		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+		transactionManager.setDataSource(dataSource);
+		return transactionManager;
+	}
+}
