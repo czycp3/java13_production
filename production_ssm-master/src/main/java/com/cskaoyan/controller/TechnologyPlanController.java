be5ae21b09a417fb660ab50481fada648d09b370
chenlongyuan <1589823959@qq.com>
2019-05-20 11:10:09 +0800

    陈珑元 修复接口

--- production_ssm-master/src/main/java/com/cskaoyan/controller/TechnologyPlanController.java
+++ production_ssm-master/src/main/java/com/cskaoyan/controller/TechnologyPlanController.java
@@ -1,5 +1,6 @@
 package com.cskaoyan.controller;
 
+import com.cskaoyan.bean.BaseResultVo;
 import com.cskaoyan.bean.QueryStatus;
 import com.cskaoyan.bean.TechnologyPlan;
 import com.cskaoyan.service.TechnologyPlanService;
@@ -15,7 +16,22 @@ public class TechnologyPlanController {
     @Autowired
     TechnologyPlanService technologyPlanService;
 
-
+//    //------查询列表--------
+//    @RequestMapping("/technologyPlan/list")
+//    @ResponseBody
+//    public BaseResultVo technology(int page, int rows){
+//        BaseResultVo baseResultVo = new BaseResultVo();
+//        List<TechnologyPlan> technologyPlans = technologyPlanService.selectAllTechnologyPlan(page,rows);
+//        int total = technologyPlanService.selectCountTechnologyPlan();
+//        baseResultVo.setRows(technologyPlans);
+//        baseResultVo.setTotal(total);
+//        return baseResultVo;
+//    }
+//
+//    @RequestMapping("/technologyPlan/find")
+//    public String technologyList(){
+//        return "technologyPlan_list";
+//    }
 
 
 }
